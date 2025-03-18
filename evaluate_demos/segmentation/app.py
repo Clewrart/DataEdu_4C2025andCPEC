266547faca1b565179fdacd3c49b67c14429f7a9
siyu <siyu_7805@163.com>
2025-03-18 20:55:02 +0800

    闫改版

--- evaluate_demos/segmentation/app.py
+++ evaluate_demos/segmentation/app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from segmentation import *
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 @app.route('/segmentation', methods=["POST"])
@@ -26,11 +26,13 @@ def seg():
             total_dice += dice
             print(dice)
     # 百分制化分数
-    score = round(total_dice/len(answer_file_dict) * 10000 + 50)
+    #score = round(total_dice/len(answer_file_dict) * 10000 + 50)
+    #score = (1.0 * total_dice / len(answer_file_dict)) * 100
+    score = round(total_dice / len(answer_file_dict) * 100,2)
     return {
         "score": score
     }
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- evaluate_demos/segmentation/segmentation.py
+++ evaluate_demos/segmentation/segmentation.py
@@ -2,7 +2,15 @@ import os
 import numpy as np
 from skimage.io import imread
 import io
+from skimage.color import rgb2gray
 
+def read_image(file):
+    img = imread(file)
+    if img.shape[-1] == 4:#如果是RGBA（4通道），去掉Alpha通道
+        img = img[:,:,:3]#取RGB三通道
+    if len(img.shape) == 3:#如果是RGB
+        img = rgb2gray(img)#转换为灰度图
+    return (img > 0.5).astype(np.uint8)#二值化
 
 def getAnswerFileDict(directory):
     file_dict = {}
@@ -10,7 +18,7 @@ def getAnswerFileDict(directory):
         for file in files:
             file_path = os.path.join(root, file)
             with open(file_path, 'rb') as f:  # 以二进制读模式打开文件
-                file_dict[file] = imread(f)
+                file_dict[file] = read_image(f)
     return file_dict
 
 
@@ -18,14 +26,19 @@ def getStudentFileDict(imgs):
     res = {}
     for img in imgs:
         img_stream = io.BytesIO(img.read())
-        res[img.filename] = imread(img_stream)
+        res[img.filename] = read_image(img_stream)
 
     return res
 
 
 def dice_coefficient(y_true, y_pred):
-    intersection = np.sum(y_true * y_pred)
-    union = np.sum(y_true) + np.sum(y_pred)
+    #如果是灰度图，则二值化处理
+    if y_true.max() > 1:
+        y_true = (y_true > 128).astype(np.uint8)
+        y_pred = (y_pred > 128).astype(np.uint8)
+
+    intersection = np.sum(y_true * y_pred)#计算像素级的交集
+    union = np.sum(y_true) + np.sum(y_pred)#计算并集
     return 2.0 * intersection / union if union > 0 else 1.0
 
 
